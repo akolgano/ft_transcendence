641ffd722ab1c12e8bf1ee9857e90e5f86252436
Juliette Barbay <barbayjuliette95@gmail.com>
2024-08-30 21:01:25 +0800

    Signing up improved + redirections

--- frontend/js/signup.js
+++ frontend/js/signup.js
@@ -1,42 +1,42 @@
+{
+	console.log("SCRIPT SIGN UP")
 
-console.log("SCRIPT SIGN UP")
+	const signupForm = document.getElementById("signupForm");
 
-const signupForm = document.getElementById("signupForm");
+	signupForm.addEventListener("submit", async (e) => {
+		e.preventDefault();
+		const formData = new FormData(signupForm);
 
-signupForm.addEventListener("submit", async (e) => {
-	e.preventDefault();
-	const formData = new FormData(signupForm);
-
-	try {
-		let errorMessage = "";
-		const response = await fetch("http://127.0.0.1:8000/signup", {
-			method: 'POST',
-			body: formData,
-		})
-		const data = await response.json();
-		if (!response.ok) {
-			// console.log("Error response body error:", data.error);
-			if (Array.isArray(data.error)) {
-				data.error.forEach((message, index) => {
-					errorMessage += "\n • ";
-					errorMessage += message;
-				});
+		try {
+			let errorMessage = "";
+			const response = await fetch("http://127.0.0.1:8000/signup", {
+				method: 'POST',
+				body: formData,
+			})
+			const data = await response.json();
+			if (!response.ok) {
+				// console.log("Error response body error:", data.error);
+				if (Array.isArray(data.error)) {
+					data.error.forEach((message, index) => {
+						errorMessage += "\n • ";
+						errorMessage += message;
+					});
+				}
+				else
+					errorMessage = data.error;
+				throw new Error(errorMessage || 'An error occurred');
+			}
+			if (data.token)
+			{
+				alert("Sign up successful!");
+				urlRoute({ target: { href: '/' }, preventDefault: () => {} });
 			}
 			else
-				errorMessage = data.error;
-			throw new Error(errorMessage || 'An error occurred');
+			{
+				alert("Sign up failed: " + data.message);
+			}
+		} catch (error) {
+			alert("Sign up failed: " + error.message);
 		}
-		console.log("Data: " + data);
-		if (data.token)
-		{
-			alert("Sign up successful!");
-			urlRoute({ target: { href: '/friends' }, preventDefault: () => {} });
-		}
-		else
-		{
-			alert("Sign up failed: " + data.message);
-		}
-	} catch (error) {
-		alert("Sign up failed: " + error.message);
-	}
-})
+	})
+}
--- frontend/js/urlRouter.js
+++ frontend/js/urlRouter.js
@@ -41,13 +41,18 @@ const urlRoutes = {
 		description: "Sign up to play pong",
 		scripts: ["../js/signup.js"]
 	},
+	"/login": {
+		template: "../templates/login.html",
+		title: "Log in",
+		description: "Log in to play pong",
+		scripts: ["../js/login.js"]
+	},
 };
 
 // create a function that watches the url and calls the urlLocationHandler
 const urlRoute = (event) => {
 	event = event || window.event; // get window.event if event argument not provided
 	event.preventDefault();
-	// window.history.pushState(state, unused, target link);
 	window.history.pushState({}, "", event.target.href);
 	urlLocationHandler();
 };
@@ -55,15 +60,12 @@ const urlRoute = (event) => {
 // create a function that handles the url location
 const urlLocationHandler = async () => {
 	const location = window.location.pathname; // get the url path
-	// if the path length is 0, set it to primary page route
 	if (location.length == 0) {
 		location = "/";
 	}
-	// get the route object from the urlRoutes object
+	// Get the route, get the html, add it to the div
 	const route = urlRoutes[location] || urlRoutes["404"];
-	// get the html from the template
 	const html = await fetch(route.template).then((response) => response.text());
-	// set the content of the content div to the html
 	document.getElementById("content").innerHTML = html;
 
 	// Load scripts
@@ -93,7 +95,6 @@ const loadScripts = (scripts) => {
 
 // add an event listener to the window that watches for url changes
 window.onpopstate = urlLocationHandler;
-// call the urlLocationHandler function to handle the initial url
 window.route = urlRoute;
 // call the urlLocationHandler function to handle the initial url
 urlLocationHandler();
