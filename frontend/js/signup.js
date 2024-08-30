0a7d59508f49e3683d45445b54248799151bc805
Juliette Barbay <barbayjuliette95@gmail.com>
2024-08-30 17:44:00 +0800

    redirect after signup okay

--- frontend/js/signup.js
+++ frontend/js/signup.js
@@ -8,7 +8,6 @@ signupForm.addEventListener("submit", async (e) => {
 	const formData = new FormData(signupForm);
 
 	try {
-		console.log(formData);
 		let errorMessage = "";
 		const response = await fetch("http://127.0.0.1:8000/signup", {
 			method: 'POST',
@@ -16,8 +15,7 @@ signupForm.addEventListener("submit", async (e) => {
 		})
 		const data = await response.json();
 		if (!response.ok) {
-			console.log("Error response body error:", data.error);
-
+			// console.log("Error response body error:", data.error);
 			if (Array.isArray(data.error)) {
 				data.error.forEach((message, index) => {
 					errorMessage += "\n • ";
@@ -26,23 +24,13 @@ signupForm.addEventListener("submit", async (e) => {
 			}
 			else
 				errorMessage = data.error;
-
 			throw new Error(errorMessage || 'An error occurred');
 		}
 		console.log("Data: " + data);
 		if (data.token)
 		{
-			console.log("Sign up successful!");
 			alert("Sign up successful!");
-			// window.location.href = "index.html";
-			// const event = new MouseEvent('click', { bubbles: true, cancelable: true});
-			// const link = document.createElement('a');
-			// link.href = "/friends";
-			// link.classList.add("spa");
-			// link.dispatchEvent(event);
-			document.getElementById("content").innerHTML = "<p>Sign up successful</p>"
-
-			// urlRoute({ target: { href: '/friends' } });
+			urlRoute({ target: { href: '/friends' }, preventDefault: () => {} });
 		}
 		else
 		{
