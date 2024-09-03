1321c3121164a290c0fe9c50df363aa22197cac7
Juliette Barbay <barbayjuliette95@gmail.com>
2024-09-04 00:13:40 +0800

    README + reorganized

---
+++ README.md
@@ -0,0 +1,35 @@
+# How to run
+
+## Backend
+
+
+```
+docker-compose down
+docker-compose build --no-cache
+docker-compose up
+```
+
+The Makefile is currently not working
+
+
+## Frontend
+
+### Go to frontend folder
+
+```
+cd frontend
+```
+
+### Run server on port 3000
+
+```
+python3 -m http.server 3000
+```
+
+### Go to http://localhost:3000/
+
+
+## Check the database for tests
+
+You can go to localhost:8000/admin to manage the database.
+User and password: admin
--- frontend/js/urlRouter.js
+++ frontend/js/urlRouter.js
@@ -1,127 +1,13 @@
 // To make it SPA
 // ADD your new page in the urlRoutes, and and the spa class to link.
 
-
 // Make it more efficient by NOT putting the event listener on every click of the document.
 // BUT make sure the content added to the DOM will be inside the class we used for capturing events.
-
 // --> When I add an element to the DOM, I add an eventListener to it
-
-// document.addEventListener("click", (e) => {
-	// 	const target = e.target;
-	// 	if (!target.classList.contains("spa"))
-	// 		return;
-// 	e.preventDefault();
-// 	urlRoute(e);
-// })
-// document.addEventListener("click", spaHandler)
-
-
-if (localStorage.getItem("token"))
-{
-		const nav = '<li class="nav-item"><p class="navbar-text m-0 px-4">Welcome, Juliette!</p></li><li class="nav-item dropdown"><a href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><img src="./images/profile_pic.jpeg" alt="avatar" class="rounded-circle border-1 avatar"></a><ul class="dropdown-menu dropdown-menu-end"><li><a class="dropdown-item" href="#">Profile</a></li><li><a class="dropdown-item" href="#">Another action</a></li><li><a class="dropdown-item" href="#" id="logout">Logout</a></li></ul>'
-		document.getElementById("nav-log").innerHTML = nav;
-		const script = document.createElement("script");
-		script.src = "../js/logout.js";
-
-		document.body.appendChild(script);
-}
-else
-{
-	const nav = '<a class="btn btn-outline-secondary spa" type="button" href="/signup">Sign up</a><a class="btn btn-outline-secondary spa mx-2" type="button" href="/login">Log in</a>'
-	document.getElementById("nav-log").innerHTML = nav;
-}
-
-function spaHandler(e) {
-	const target = e.target;
-	e.preventDefault();
-	urlRoute(e);
-}
-
-function makeItSpa() {
-
-	let nav = document.querySelectorAll(".spa")
-
-	nav.forEach( link => {
-		link.addEventListener("click", spaHandler)
-	})
-}
-
-makeItSpa();
-
 console.log("ROUTER");
 
+// ---------------------------------------- VARIABLES FOR SPA ----------------------------------------
 
-// Function that watches the url and calls the urlLocationHandler
-const urlRoute = (event) => {
-	if (localStorage.getItem("token") == null && urlRoutes[window.location.pathname].auth == true)
-		href = "http://localhost:3000/login"
-
-	window.history.pushState({}, "", event.target.href);
-	urlLocationHandler();
-};
-
-// Function that handles the url location
-const urlLocationHandler = async () => {
-
-	let location = window.location.pathname;
-
-	if (location.length == 0)
-		location = "/";
-
-	// Not logged in and route needs authentication
-	if (localStorage.getItem("token") == null && urlRoutes[location].auth == true) // User is logged out
-	{
-		console.log("Needs to be logged in")
-		location = "/login"
-	}
-
-	// Get the route, get the html, add it to the div
-	const route = urlRoutes[location] || urlRoutes["404"];
-	content.innerHTML = route.content;
-
-	//  Add eventListener on new content
-	links = content.querySelectorAll(".spa");
-	links.forEach( link => {
-		link.addEventListener("click", spaHandler)
-	})
-
-	// Load scripts
-	loadScripts(route.scriptContent || []);
-
-	// metadata
-	document.title = route.title;
-	document
-	.querySelector('meta[name="description"]')
-	.setAttribute("content", route.description);
-};
-
-const addScripts = (scripts) => {
-	scripts.forEach(scriptSrc => {
-		const script = document.createElement("script");
-		script.dataset.pageScript = "";
-		script.textContent = scriptSrc;
-		document.body.appendChild(script);
-	});
-}
-
-
-const loadScripts = (scripts) => {
-	// Remove old scripts
-	const existingScripts = document.querySelectorAll("script[data-page-script]");
-	existingScripts.forEach(script => script.remove());
-
-	// Add new scripts
-	addScripts(scripts)
-};
-
-// Ensures correct routing when using back/forward buttons from history
-window.onpopstate = urlLocationHandler;
-
-// Make the urlRoute function globally accessible.
-window.route = urlRoute;
-
-// call the urlLocationHandler function to handle the initial url
 const urlRoutes = {
 	404: {
 		template: "./404.html",
@@ -169,6 +55,87 @@ const urlRoutes = {
 
 let logoutScript;
 
+// ---------------------------------------- FUNCTIONS ----------------------------------------
+
+function spaHandler(e) {
+	const target = e.target;
+	e.preventDefault();
+	urlRoute(e);
+}
+
+function makeItSpa() {
+
+	let nav = document.querySelectorAll(".spa")
+
+	nav.forEach( link => {
+		link.addEventListener("click", spaHandler)
+	})
+}
+
+// Function that watches the url and calls the urlLocationHandler
+const urlRoute = (event) => {
+	// let href = event.target.href
+	// if (localStorage.getItem("token") == null && urlRoutes[window.location.pathname].auth == true)
+	// {
+	// 	href = "http://localhost:3000/login"
+	// 	console.log("here")
+	// }
+
+	window.history.pushState({}, "", event.target.href);
+	urlLocationHandler();
+};
+
+// Function that handles the url location
+const urlLocationHandler = async () => {
+
+	let location = window.location.pathname;
+
+	if (location.length == 0)
+		location = "/";
+
+	// Not logged in and route needs authentication
+	if (localStorage.getItem("token") == null && urlRoutes[location].auth == true) // User is logged out
+	{
+		console.log("Needs to be logged in")
+		location = "/login"
+	}
+
+	// Get the route, get the html, add it to the div
+	const route = urlRoutes[location] || urlRoutes["404"];
+	content.innerHTML = route.content;
+
+	//  Add eventListener on new content
+	let links = content.querySelectorAll(".spa");
+	links.forEach( link => {
+		link.addEventListener("click", spaHandler)
+	})
+
+	loadScripts(route.scriptContent || []);
+
+	document.title = route.title;
+	document
+	.querySelector('meta[name="description"]')
+	.setAttribute("content", route.description);
+};
+
+const addScripts = (scripts) => {
+	scripts.forEach(scriptSrc => {
+		const script = document.createElement("script");
+		script.dataset.pageScript = "";
+		script.textContent = scriptSrc;
+		document.body.appendChild(script);
+	});
+}
+
+const loadScripts = (scripts) => {
+	// Remove old scripts
+	const existingScripts = document.querySelectorAll("script[data-page-script]");
+	existingScripts.forEach(script => script.remove());
+
+	// Add new scripts
+	addScripts(scripts)
+};
+
 // Fetch all the pages already to have everything on the client side.
 const fetchTemplate = async (route) => {
 	try {
@@ -184,20 +151,44 @@ const fetchTemplate = async (route) => {
 			route.scriptContent.push(scriptText);
 		}
 
-
 	} catch (error) {
 		console.log("Error fetching " + route.title + error)
 	}
-
-	const logoutResponse = await fetch("../js/logout.js")
-	logoutScript = await logoutResponse.text()
 }
 
 const fetchPages = async () => {
 	for (let route in urlRoutes) {
 		await fetchTemplate(urlRoutes[route])
 	}
+	const logoutResponse = await fetch("../js/logout.js")
+	logoutScript = await logoutResponse.text()
+
+	// call the urlLocationHandler function to handle the initial url
 	urlLocationHandler();
 }
 
-fetchPages();
+
+// ---------------------------------------- EXECUTION ----------------------------------------
+const run = async () => {
+	await fetchPages();
+
+	if (localStorage.getItem("token"))
+	{
+			const nav = `<li class="nav-item"><p class="navbar-text m-0 px-4">Welcome, ${localStorage.getItem("user")}</p></li><li class="nav-item dropdown"><a href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><img src="./images/profile_pic.jpeg" alt="avatar" class="rounded-circle border-1 avatar"></a><ul class="dropdown-menu dropdown-menu-end"><li><a class="dropdown-item" href="#">Profile</a></li><li><a class="dropdown-item" href="#">Another action</a></li><li><a class="dropdown-item" href="#" id="logout">Logout</a></li></ul>`
+			document.getElementById("nav-log").innerHTML = nav;
+			const script = document.createElement("script");
+			script.src = "../js/logout.js";
+			document.body.appendChild(script);
+	}
+	else
+	{
+		const nav = '<a class="btn btn-outline-secondary spa" type="button" href="/signup">Sign up</a><a class="btn btn-outline-secondary spa mx-2" type="button" href="/login">Log in</a>'
+		document.getElementById("nav-log").innerHTML = nav;
+	}
+
+	makeItSpa();
+	window.onpopstate = urlLocationHandler; // Ensures correct routing when using back/forward buttons from history
+	window.route = urlRoute; // Make the urlRoute function globally accessible.
+}
+
+run()
