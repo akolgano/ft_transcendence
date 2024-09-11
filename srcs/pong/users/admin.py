dabf1ac214573c9928640d2602940b87c2212c04
akolgano <anna080688@gmail.com>
2024-09-11 20:32:37 +0800

    User API

---
+++ srcs/pong/users/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.html import format_html
+
+class UserAdmin(BaseUserAdmin):
+
+    # Display profile picture in list view
+    list_display = ('username', 'email', 'profile_picture_display')
+
+    def profile_picture_display(self, obj):
+        if obj.profile_picture:
+            return format_html('<img src="{}" style="width: 60px; height: 50px;"/>', obj.profile_picture.url)
+        #return format_html('<img src="{}" style="width: 50px; height: 50px;"/>', '/media/profile_pictures/default.jpg')
+    
+    profile_picture_display.short_description = 'Profile Picture'
+    model = CustomUser
+    fieldsets = BaseUserAdmin.fieldsets + (
+        (None, {'fields': ('profile_picture',)}),
+    )
+    add_fieldsets = BaseUserAdmin.add_fieldsets + (
+        (None, {'fields': ('profile_picture',)}),
+    )
+
+admin.site.register(CustomUser, UserAdmin)
