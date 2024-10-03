ac104a168011c62adcfc4096ccc869bbf640e69b
Juliette Barbay <barbayjuliette95@gmail.com>
2024-10-03 17:41:07 +0800

    Form validation sign up

--- frontend/js/addFriend.js
+++ frontend/js/addFriend.js
@@ -1,6 +1,6 @@
 console.log("ADD FRIEND SCRIPT")
 
-const getErrorKeyAddFriend = (error) =>
+function getErrorKeyAddFriend(error)
 {
 	let errorKey;
 	console.log("Error: " + error);
--- frontend/js/signup.js
+++ frontend/js/signup.js
@@ -1,4 +1,81 @@
-{
+
+function resetErrorField() {
+	const errorDivs = document.querySelectorAll(".form-error");
+
+	errorDivs.forEach(div => {
+		div.innerHTML = ""
+	});
+}
+
+function checkPasswordMatch() {
+	let newPassword = document.getElementById("password").value;
+	let repeatPassword = document.getElementById("confirm-password").value;
+
+	if (newPassword !== repeatPassword)
+	{
+		const errorPassword = document.querySelector(".repeat-password-error");
+		let errorTag = document.createElement("p");
+		errorTag.innerHTML = "Passwords do not match";
+		errorPassword.appendChild(errorTag);
+		return (false);
+	}
+	return (true);
+}
+
+function addErrorToHTML(data) {
+	for (const key in data)
+	{
+		let errorClass;
+		if (key == "error")
+			errorClass = ".password-error";
+		else
+			errorClass = `.${key}-error`;
+
+		const errorDiv = document.querySelector(errorClass);
+
+		data[key].forEach(message => {
+			let errorTag = document.createElement("p");
+			errorTag.innerHTML = message;
+			errorDiv.appendChild(errorTag);
+		});
+	}
+}
+
+async function addEventSignUpForm(e) {
+
+	e.preventDefault();
+	resetErrorField();
+	if (!checkPasswordMatch())
+		return ;
+
+	const formData = new FormData(signupForm);
+	try {
+		const response = await fetch("http://127.0.0.1:8000/signup", {
+			method: 'POST',
+			body: formData,
+		})
+		const data = await response.json();
+		if (!response.ok) {
+			// console.log("Data from back: " + JSON.stringify(data));
+			addErrorToHTML(data);
+			throw new Error(JSON.stringify(data) || 'An error occurred');
+		}
+		if (data.token)
+		{
+			displayAlert("auth.sign-up-success", "success");
+			urlRoute({ target: { href: '/' }, preventDefault: () => {} });
+		}
+		else
+		{
+			displayAlert("auth.error", "danger");
+			console.log(data.message);
+		}
+	} catch (error) {
+		console.log(error.message)
+	}
+}
+
+function signUpScript() {
 	console.log("SCRIPT SIGN UP")
 
 	const signupForm = document.getElementById("signupForm");
@@ -6,51 +83,10 @@
 	document.querySelectorAll(".toggle-password").forEach(node => {
 		node.addEventListener("click", togglePassword)
 	});
-
-	signupForm.addEventListener("submit", async (e) => {
-		e.preventDefault();
-
-		let newPassword = document.getElementById("password").value;
-		let repeatPassword = document.getElementById("confirm-password").value;
-
-		if (newPassword !== repeatPassword)
-		{
-			alert("Passwords do not match");
-			return ;
-		}
-
-		const formData = new FormData(signupForm);
-		try {
-			let errorMessage = "";
-			const response = await fetch("http://127.0.0.1:8000/signup", {
-				method: 'POST',
-				body: formData,
-			})
-			const data = await response.json();
-			if (!response.ok) {
-				// console.log("Data from back: " + Object.values(data));
-				let errors_array = Object.values(data);
-				errors_array.forEach((message, index) => {
-					errorMessage += "\n • ";
-					errorMessage += message;
-				});
-				throw new Error(errorMessage || 'An error occurred');
-			}
-			if (data.token)
-			{
-				alert(translator.translateForKey("auth.sign-up-success", siteLanguage))
-				urlRoute({ target: { href: '/' }, preventDefault: () => {} });
-			}
-			else
-			{
-				// TO DO: Get the error message in english, then check what it is, depending on that, translate it.
-				// console.log("Language: " + siteLanguage)
-				alert(translator.translateForKey("auth.error", siteLanguage) + data.message)
-				console.log(data.message);
-			}
-		} catch (error) {
-			alert(translator.translateForKey("auth.error", siteLanguage) + error.message)
-			console.log(error.message)
-		}
-	})
+	signupForm.addEventListener("submit", addEventSignUpForm);
 }
+
+signUpScript();
+
+
+// Data from back: {"username":["A user with that username already exists."],"email":["Enter a valid email address."]}
