3aa022079afb7cefe9f59734a5847425c8d5a43a
Juliette Barbay <barbayjuliette95@gmail.com>
2024-10-07 15:04:16 +0800

    CSS changed to Bootstrap

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -17,7 +17,7 @@ const togglePassword = (event) => {
 
 const displayAlert = (key, type) => {
 	const content = document.querySelector("#content");
-	const alert = `<div class="alert alert-${type} alert-dismissible fade show" role="alert">
+	const alert = `<div class="alert alert-${type} alert-dismissible fade show position-absolute" role="alert">
 	${translator.translateForKey(key, siteLanguage)}
 	<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>`
