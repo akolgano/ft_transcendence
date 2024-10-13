ce91c0a91754c5ce7b16c9c939c262456550e66d
Juliette Barbay <barbayjuliette95@gmail.com>
2024-10-13 18:47:47 +0800

    Change alerts so translation when changing language

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -22,10 +22,11 @@ const displayAlert = (key, type) => {
 		key = "error-fetch";
 	const content = document.querySelector("#content");
 	const alert = `<div class="alert alert-${type} alert-dismissible fade show position-absolute" role="alert">
-	${translator.translateForKey(key, siteLanguage)}
+	<span data-i18n="${key}"></span>
 	<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>`
 	content.insertAdjacentHTML("beforebegin", alert);
+	translateNewContent(document.querySelector(".alert"));
 }
 
 const removeAlert = () => {
