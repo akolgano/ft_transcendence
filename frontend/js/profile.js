64f523f575ecf7500b388fe6f78b3cb5680895f4
Juliette Barbay <barbayjuliette95@gmail.com>
2024-10-13 17:41:28 +0800

    Add online status to Profile page

--- frontend/js/profile.js
+++ frontend/js/profile.js
@@ -1,12 +1,4 @@
 
-function addBaseDataHistory(params) {
-	let user = JSON.parse(localStorage.getItem("user"));
-	console.log(user);
-
-	document.querySelector('.card-username').innerHTML = user.username
-	document.querySelector(".profile-pic").src = "http://localhost:8000" + user.profile_picture
-}
-
 function formatDate(date) {
 
 	const utcDate = new Date(date)
@@ -26,31 +18,64 @@ function formatDate(date) {
 	return (formattedDate);
 }
 
-function addResultsToHTML(data) {
+function addGameResults(game_results) {
+
+	const gameResultsDiv = document.getElementById("game-history-container")
+
+	if (game_results.length == 0)
+	{
+		const gameHTML = "<p>No game results yet</p>"
+		gameResultsDiv.insertAdjacentHTML("beforeend", gameHTML);
+	}
+	else
+	{
+		game_results.forEach(game => {
+			const result = (game.score[0] > game.score[1] ? "🏆" : "🥀")
+			const date = formatDate(game.date_time)
+
+			const gameHTML = `
+			<div class="game-card border rounded bg-light w-100 d-inline-block p-1 mb-2 bg-success-subtle">
+				<div class="d-flex justify-content-between px-2">
+					<p class="my-1 me-5">${result}&nbsp;&nbsp;${game.score[0]} - ${game.score[1]}</p>
+					<p class="my-1 flex-grow-1">${game.opponent_username}</p>
+					<p class="my-1">${date}</p>
+				</div>
+			</div>`
+			gameResultsDiv.insertAdjacentHTML("beforeend", gameHTML);
+
+		});
+	}
+}
+
+function updateProfileCard(data)
+{
 	let points = (data.victories * 10) - (data.losses * 5);
 	if (points < 0) points = 0;
 
+	document.querySelector('.card-username').innerHTML = data.username
+	document.querySelector(".profile-pic").src = "http://localhost:8000" + data.profile_picture
 	document.querySelector(".user-points").innerHTML = `&nbsp;${points}&nbsp;`
 	document.querySelector(".user-victories").innerHTML = `&nbsp;${data.victories}&nbsp`
 	document.querySelector(".user-losses").innerHTML = `&nbsp;${data.losses}&nbsp`
-	const gameResults = document.getElementById("game-history-container")
 
+	if (data.username !== JSON.parse(localStorage.getItem("user")).username)
+	{
+		const online_text = document.querySelector(".online-status")
 
-	data.game_results.forEach(game => {
-		const result = (game.score[0] > game.score[1] ? "🏆" : "🥀")
-		const date = formatDate(game.date_time)
+		// TO DO delete this line:
+		data.online_status = false;
 
-		const gameHTML = `
-		<div class="game-card border rounded bg-light w-100 d-inline-block p-1 mb-2 bg-success-subtle">
-			<div class="d-flex justify-content-between px-2">
-				<p class="my-1 me-5">${result}&nbsp;&nbsp;${game.score[0]} - ${game.score[1]}</p>
-				<p class="my-1 flex-grow-1">${game.opponent_username}</p>
-				<p class="my-1">${date}</p>
-			</div>
-		</div>`
-		gameResults.insertAdjacentHTML("beforeend", gameHTML);
+		if (data.online_status === true)
+			online_text.innerText = `🟢 ${translator.translateForKey("profile.online", siteLanguage)}`
+		else
+			online_text.innerText = `🔴 ${translator.translateForKey("profile.offline", siteLanguage)}`
+	}
+}
 
-	});
+function addResultsToHTML(data) {
+
+	updateProfileCard(data);
+	addGameResults(data.game_results);
 }
 
 function getArgument() {
@@ -71,9 +96,9 @@ function getArgument() {
 }
 
 
-async function fetchHistory() {
+async function fetchHistory(urlArgument) {
 	removeAlert();
-	const urlArgument = getArgument();
+
 	try {
 		const response = await fetch(`https://localhost/api/player/stats/${urlArgument}`, {
 			headers: {
@@ -113,5 +138,9 @@ async function fetchHistory() {
 	}
 }
 
-addBaseDataHistory();
-fetchHistory();
+function profileScript() {
+	const urlArgument = getArgument();
+	fetchHistory(urlArgument);
+}
+
+profileScript()
